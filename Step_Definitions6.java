9531f9d87a4aee88d3e5f999fc51c65cf4b6aefd
sayom88 <sayom88@yahoo.com>
2025-02-23 15:09:02 +0530

    Add Step_Definitions6.java

---
+++ Step_Definitions6.java
@@ -0,0 +1,107 @@
+```java
+public class StepDefinitions {
+    private WebDriver driver;
+
+    public StepDefinitions(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    /**
+     * Navigate to the PTSB mortgage calculator page
+     * @param url the URL to navigate to
+     */
+    public void navigateToMortgageCalculator(String url) {
+        driver.get(url);
+    }
+
+    /**
+     * Select buyer type
+     * @param buyerType the buyer type to select
+     */
+    public void selectBuyerType(String buyerType) {
+        driver.findElement(By.xpath("//select[@id='buyerType']")).sendKeys(buyerType);
+    }
+
+    /**
+     * Click the "Next" button
+     */
+    public void clickNextButton() {
+        driver.findElement(By.xpath("//button[@id='next']")).click();
+    }
+
+    /**
+     * Select application type
+     * @param applicationType the application type to select
+     */
+    public void selectApplicationType(String applicationType) {
+        driver.findElement(By.xpath("//select[@id='applicationType']")).sendKeys(applicationType);
+    }
+
+    /**
+     * Select marital status
+     * @param maritalStatus the marital status to select
+     */
+    public void selectMaritalStatus(String maritalStatus) {
+        driver.findElement(By.xpath("//select[@id='maritalStatus']")).sendKeys(maritalStatus);
+    }
+
+    /**
+     * Answer "No" to having dependents
+     */
+    public void answerNoToDependents() {
+        driver.findElement(By.xpath("//input[@id='noDependents']")).click();
+    }
+
+    /**
+     * Enter age
+     * @param age the age to enter
+     */
+    public void enterAge(String age) {
+        driver.findElement(By.xpath("//input[@id='age']")).sendKeys(age);
+    }
+
+    /**
+     * Enter annual income
+     * @param annualIncome the annual income to enter
+     */
+    public void enterAnnualIncome(String annualIncome) {
+        driver.findElement(By.xpath("//input[@id='annualIncome']")).sendKeys(annualIncome);
+    }
+
+    /**
+     * Select loan term
+     * @param loanTerm the loan term to select
+     */
+    public void selectLoanTerm(String loanTerm) {
+        driver.findElement(By.xpath("//select[@id='loanTerm']")).sendKeys(loanTerm);
+    }
+
+    /**
+     * Click the "Calculate" button
+     */
+    public void clickCalculateButton() {
+        driver.findElement(By.xpath("//button[@id='calculate']")).click();
+    }
+
+    /**
+     * Validate maximum borrowing amount
+     */
+    public void validateMaximumBorrowingAmount() {
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@id='maximumBorrowingAmount']")).isDisplayed());
+    }
+
+    /**
+     * Validate estimated monthly repayments
+     */
+    public void validateEstimatedMonthlyRepayments() {
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@id='estimatedMonthlyRepayments']")).isDisplayed());
+    }
+
+    /**
+     * Validate additional mortgage-related benefits
+     */
+    public void validateAdditionalMortgageRelatedBenefits() {
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@id='additionalMortgageRelatedBenefits']")).isDisplayed());
+    }
+}
+```
